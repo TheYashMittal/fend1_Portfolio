4e59e93bb55ba86415033ca98aa0814e130aebde
TheYashMittal <yash_mittal@outlook.com>
2017-03-16 15:23:29 +0530

    First Commit

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+
+module.exports = function(grunt) {
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            width: 600,
+            //height: 400,
+            suffix: 'sm',
+            quality: 30
+          }]
+        },
+
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png,jpeg}'],
+          cwd: 'images_src/',
+          dest: 'images/'
+        }]
+      }
+    },
+
+    /* Clear out the images directory if it exists */
+    clean: {
+      dev: {
+        src: ['images'],
+      },
+    },
+
+    /* Generate the images directory if it is missing */
+    mkdir: {
+      dev: {
+        options: {
+          create: ['images']
+        },
+      },
+    },
+
+  });
+
+//  grunt.loadNpmTasks('grunt-responsive-images');
+//  grunt.loadNpmTasks('grunt-contrib-clean');
+//  grunt.loadNpmTasks('grunt-contrib-copy');
+//  grunt.loadNpmTasks('grunt-mkdir');
+    grunt.registerTask('default', ['clean', 'mkdir', 'responsive_images']);
+//  grunt.registerTask('default', ['clean', 'mkdir', 'responsive_images']);
+
+};
